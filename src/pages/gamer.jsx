43ff6b1e4c3f6e989c0a1abfcab960226367b82e
john-illimani <illimanijohn09@gmail.com>
2026-08-02 19:37:47 -0400

    version de quipos 20

--- src/pages/gamer.jsx
+++ src/pages/gamer.jsx
@@ -251,7 +251,7 @@ const BRACKET_DATA = [
     name: "Grupo C",
     day: "Domingo",
     accent: "violet",
-    winners: ["Land Stand"], // 👈 pon aquí los ganadores de este grupo
+    winners: ["Land Stand","Spearhead"], // 👈 pon aquí los ganadores de este grupo
     matches: [
 
       { time: "7:00 PM", team1: "Land Stand", team2: "Nelore ", logo1: "", logo2: "" },
