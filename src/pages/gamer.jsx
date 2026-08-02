13acee648fefab7080624cd94452633c23924859
john-illimani <illimanijohn09@gmail.com>
2026-08-01 20:31:40 -0400

    version de quipos 9

--- src/pages/gamer.jsx
+++ src/pages/gamer.jsx
@@ -216,7 +216,7 @@ const TOURNAMENT = {
 const NEXT_MATCH_DATE = new Date("2026-12-15T18:00:00");
 
 // ============================================================
-// TOURNAMENT BRACKET DATA  holaaaaa 22sss
+// TOURNAMENT BRACKET DATA  holaaaaa 22ssswerwrwer
 // ============================================================
 
 const BRACKET_DATA = [
